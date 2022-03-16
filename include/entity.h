01aeec90012171588b52e57b364575635d5fbb4a
Naachiket Pant <naachiketpant88@gmail.com>
2022-03-16 15:44:14 +0530

    Add draw function declaration to entity.h

--- include/entity.h
+++ include/entity.h
@@ -1,6 +1,9 @@
 #ifndef ENTITY_H_
 #define ENTITY_H_
 
+#include <SDL2/SDL.h>
+
+/* PADDLE */
 struct Paddle {
     int x;
     int y;
@@ -10,8 +13,12 @@ struct Paddle {
 }typedef Paddle;
 
 Paddle* createPaddle(int x, int y, int width, int height, int speed);
-void destroyPaddle(Paddle* paddle);
 
+void drawPaddle(Paddle* paddle, SDL_Renderer* renderer);
+void destroyPaddle(Paddle* paddle);
+/* PADDLE END */
+
+/* BALL */
 struct Ball {
     int x;
     int y;
@@ -23,6 +30,11 @@ struct Ball {
 }typedef Ball;
 
 Ball* createBall(int x, int y, int width, int height, int speed, int directionX, int directionY);
+void drawBall(Ball* ball, SDL_Renderer*  renderer);
 void destroyBall(Ball* ball);
+/* PADDLE END */
+
+
+void explodeAt(int x, int y, int directionX, int directionY, SDL_Renderer* renderer);
 
 #endif
